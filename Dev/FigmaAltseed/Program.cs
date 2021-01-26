400424070aa88245c45177c24c7d8c46f27c6820
NumAniCloud <num.ani.cloud@gmail.com>
2021-01-26 18:29:50 +0900

    不要なusingを削除

--- Dev/FigmaAltseed/Program.cs
+++ Dev/FigmaAltseed/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 
 namespace FigmaAltseed
 {
--- Dev/FigmaAltseed/Startup.cs
+++ Dev/FigmaAltseed/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using FigmaAltseed.Converter;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
